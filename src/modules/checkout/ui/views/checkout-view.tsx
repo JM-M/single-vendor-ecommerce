c064cd7878e5705601cf6cf29f493bd70e94d53f
Michael Jola-Moses (JMM) <jolamoses@gmail.com>
2025-05-26 17:57:44 +0100

    19-checkout-page

---
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
+import { generateTenantURL } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { InboxIcon, LoaderIcon } from "lucide-react";
+import { useCart } from "../../hooks/use-cart";
+import { CheckoutItem } from "../components/checkout-item";
+import { CheckoutSidebar } from "../components/checkout-sidebar";
+
+interface CheckoutViewProps {
+  tenantSlug: string;
+}
+
+export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
+  const { productIds, clearAllCarts, removeProduct } = useCart(tenantSlug);
+
+  const trpc = useTRPC();
+  const { data, error, isLoading } = useQuery(
+    trpc.checkout.getProducts.queryOptions({
+      ids: productIds,
+    }),
+  );
+
+  // TODO: Rather than clearing the cart, we should remove the mark invalid products
+  // as no longer available, so the user can still see them in the cart.
+  useEffect(() => {
+    if (error?.data?.code === "NOT_FOUND") {
+      clearAllCarts();
+      toast.warning("Invalid products in cart. Cart has been cleared.");
+    }
+  }, [error, clearAllCarts]);
+
+  if (isLoading) {
+    return (
+      <div className="px-4 pt-4 lg:px-12 lg:pt-16">
+        <div className="flex w-full flex-col items-center justify-center gap-y-4 rounded-lg border border-dashed border-black bg-white p-8">
+          <LoaderIcon className="tex-muted-foreground animate-spin" />
+          <p className="text-base font-medium">Loading...</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (data?.totalDocs === 0) {
+    return (
+      <div className="px-4 pt-4 lg:px-12 lg:pt-16">
+        <div className="flex w-full flex-col items-center justify-center gap-y-4 rounded-lg border border-dashed border-black bg-white p-8">
+          <InboxIcon />
+          <p className="text-base font-medium">No products found</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="px-4 pt-4 lg:px-12 lg:pt-16">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-7 lg:gap-16">
+        <div className="lg:col-span-4">
+          <div className="overflow-hidden rounded-md border bg-white">
+            {data?.docs.map((product, index) => {
+              const { id, name, price, image, tenant } = product;
+              const isLast = index === data.docs.length - 1;
+              return (
+                <CheckoutItem
+                  key={id}
+                  name={name}
+                  isLast={isLast}
+                  imageUrl={image?.url}
+                  productUrl={generateTenantURL(tenant.slug, `/products/${id}`)}
+                  tenantUrl={generateTenantURL(tenant.slug)}
+                  tenantName={tenant.name}
+                  price={price}
+                  onRemove={() => removeProduct(id)}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="lg:col-span-3">
+          <CheckoutSidebar
+            total={data?.totalPrice || 0}
+            onCheckout={() => {}}
+            isCanceled={false}
+            isPending={false}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
