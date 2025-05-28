37bded05dd9e13a5cf307b62d121c0f058f31527
Michael Jola-Moses (JMM) <jolamoses@gmail.com>
2025-05-28 07:26:43 +0100

    28: Deployment fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,12 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantURL(slug: string, childrenPath: string = "") {
-  if (process.env.NODE_ENV === "development") {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING,
+  );
+
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
     return (
       process.env.NEXT_PUBLIC_APP_URL +
       `/tenants/${slug}/${childrenPath}`.replace(/\/+/g, "/")
