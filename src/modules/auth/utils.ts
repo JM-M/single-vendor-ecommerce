7efa320dc6ba464885e77357dcbc1ef8ba7f244d
Michael Jola-Moses (JMM) <jolamoses@gmail.com>
2025-05-28 08:45:54 +0100

    31: Fix cookie bugs

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -5,16 +5,16 @@ interface Props {
   value: string;
 }
 export const generateAuthCookie = async ({ prefix, value }: Props) => {
-  const isProduction = process.env.NODE_ENV === "production";
-
   const cookies = await getCookies();
   cookies.set({
     name: `${prefix}-token`, // payload-token by default
     value,
     httpOnly: true,
     path: "/",
-    sameSite: isProduction ? "none" : undefined,
-    domain: isProduction ? process.env.NEXT_PUBLIC_ROOT_DOMAIN : undefined,
-    secure: isProduction,
+    ...(process.env.NODE_ENV !== "production" && {
+      sameSite: "none",
+      domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+      secure: true,
+    }),
   });
 };
